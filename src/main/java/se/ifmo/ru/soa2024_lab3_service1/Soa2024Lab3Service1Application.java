b0d17c504b7c13693cae2f0b275f7d9553af4a8e
artemychych <artemychmain@gmail.com>
2024-01-18 00:13:21 +0300

    Initial commit

---
+++ src/main/java/se/ifmo/ru/soa2024_lab3_service1/Soa2024Lab3Service1Application.java
@@ -0,0 +1,13 @@
+package se.ifmo.ru.soa2024_lab3_service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soa2024Lab3Service1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Soa2024Lab3Service1Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/ifmo/ru/soa2024_lab3_service1/Soa2024Lab3Service1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ifmo.ru.soa2024_lab3_service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soa2024Lab3Service1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
